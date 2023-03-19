015cd8e259350d1ba41368b9e390a46863ba5628
Inv1x <78815019+Inv1x@users.noreply.github.com>
2023-03-19 19:31:13 +0300

    chore: linting and build

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -118,27 +118,30 @@ describe('zipSources', function () {
 });
 
 describe('lockbox', () => {
-  test("it should return right lockbox secrets",() => {
+  test('it should return right lockbox secrets', () => {
     const input = ['ENV_VAR_1=id/verId/VAR_1', 'ENV_VAR_2=id/verId/VAR_2'];
     const result = parseLockboxVariables(input);
-    const expected: Secret[] = [{
+    const expected: Secret[] = [
+      {
         environmentVariable: 'ENV_VAR_1',
         id: 'id',
         versionId: 'verId',
-        key: 'VAR_1'
+        key: 'VAR_1',
       },
       {
         environmentVariable: 'ENV_VAR_2',
         id: 'id',
         versionId: 'verId',
-        key: 'VAR_2'
-      }];
-    expect(result).toEqual(expected)
+        key: 'VAR_2',
+      },
+    ];
+    expect(result).toEqual(expected);
   });
 
-  test.each(
-      ['123412343', '123=id', '123=id/verId', '123=id/verId/']
-  )("it should throw error when bad input provided", (input) => {
-    expect(() => parseLockboxVariables(input)).toThrow();
-  });
+  test.each(['123412343', '123=id', '123=id/verId', '123=id/verId/'])(
+    'it should throw error when bad input provided',
+    input => {
+      expect(() => parseLockboxVariables(input)).toThrow();
+    },
+  );
 });
