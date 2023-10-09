37b49179e5e618f061853f9feb167bd6ed0519d1
Aleksei Anisimov <i@1xtr.dev>
2023-10-10 00:06:43 +0300

    refactor: log levels variants limited

--- src/log-level.ts
+++ src/log-level.ts
@@ -6,11 +6,13 @@ export const {
   },
 } = cloudApi;
 
+const LOG_LEVEL_VALUES = ['TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'];
+
 export const parseLogLevel = (levelKey: string): cloudApi.logging.log_entry.LogLevel_Level => {
   if (levelKey === '') {
     return LogLevel_Level.LEVEL_UNSPECIFIED;
   }
-  if (!Object.keys(LogLevel_Level).includes(levelKey.toUpperCase())) {
+  if (!LOG_LEVEL_VALUES.includes(levelKey.toUpperCase())) {
     throw new Error('Log level has unknown value');
   }
   return LogLevel_Level[levelKey.toUpperCase() as keyof typeof LogLevel_Level];
