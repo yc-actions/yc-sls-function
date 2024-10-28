bf5130132129608a1f29e07f251882e4afe76803
Ivan Tezin <ivantezin2@gmail.com>
2024-10-28 18:38:06 +0300

    fix: remove join & remove rest for value

--- src/main.ts
+++ src/main.ts
@@ -330,8 +330,8 @@ function parseEnvironmentVariables(env: string[]): { [s: string]: string } {
 
     const environment: { [key: string]: string } = {}
     for (const line of env) {
-        const [key, ...value] = line.split(/=(.*)/s)
-        environment[key.trim()] = value.join()?.trim()
+        const [key, value] = line.split(/=(.*)/s)
+        environment[key.trim()] = value.trim()
     }
 
     core.info(`EnvObject: "${JSON.stringify(environment)}"`)
