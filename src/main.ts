6cec35fe9bc6ee5d00b0f854f17deb3dd9251c6a
Ivan Tezin <ivantezin2@gmail.com>
2024-10-28 14:50:27 +0300

    fix: splitting multiline variables

--- src/main.ts
+++ src/main.ts
@@ -330,7 +330,7 @@ function parseEnvironmentVariables(env: string[]): { [s: string]: string } {
 
     const environment: { [key: string]: string } = {}
     for (const line of env) {
-        const [key, ...value] = line.split('=')
+        const [key, ...value] = line.split(/=(.*)/s)
         environment[key.trim()] = value.join()?.trim()
     }
 
