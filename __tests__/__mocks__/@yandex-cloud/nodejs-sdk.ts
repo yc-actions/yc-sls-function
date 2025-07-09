63eb3db35349f5580ea324bcb0328079897e048f
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2025-07-09 12:05:02 +0200

    test: update main test to use centralized mocks and refactor run function calls

--- __tests__/__mocks__/@yandex-cloud/nodejs-sdk.ts
+++ __tests__/__mocks__/@yandex-cloud/nodejs-sdk.ts
@@ -8,6 +8,7 @@ import {
 } from './nodejs-sdk/serverless-functions-v1'
 import { __setServiceAccountList, ServiceAccountServiceMock } from './nodejs-sdk/iam-v1'
 import { __setLockboxVersions, LockboxSecretServiceMock } from './nodejs-sdk/lockbox-v1'
+export { errors } from '@yandex-cloud/nodejs-sdk'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const sdk: any = jest.createMockFromModule('@yandex-cloud/nodejs-sdk')
